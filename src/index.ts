3857e1552d86bf41c640e6ccd9a0c47de4de1604
brennanelson <brenna@basicinvite.com>
2022-05-12 13:44:24 -0600

    Creating Base manager.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export const Greeting = (name: string) => `Hello ${name}`;
+import { Manager } from "./Utility/Manager";
+
+export const Base = new Manager({ trackingCode: "test", trackingId: "test" });
