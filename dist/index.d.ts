596055e6e97a46f411b788bdab37bdc97c574efa
brennanelson <brenna@basicinvite.com>
2022-10-03 14:12:23 -0600

    Fixing entrypoint to give access to interfaces.

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import { AnalyticsEvent } from "Objects/AnalyticsEvent";
+import { AnalyticsEventDataInterface } from "Objects/Interfaces/AnalyticsEventData.Interface";
+import { AnalyticsEventInterface } from "Objects/Interfaces/AnalyticsEvent.Interface";
+import { ManagerConfig } from "Configuration/ManagerConfig";
+import AnalyticsPluginManager from "Plugins/AnalyticsPluginManager";
+import ManagerPlugin from "Utility/ManagerPlugin";
+export { AnalyticsEvent, AnalyticsEventDataInterface, AnalyticsEventInterface, ManagerConfig, AnalyticsPluginManager, ManagerPlugin };
